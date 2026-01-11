3e3b0dd2f46c1f2a30ee32543647d10dacee25e0
abdul-mueed-shz <abdulmueedshahbaz@gmail.com>
2026-01-11 13:30:31 +0100

    remove unnecessary endpoint

--- src/main/java/com/abdul/checkinservice/adapter/in/web/TimesheetController.java
+++ src/main/java/com/abdul/checkinservice/adapter/in/web/TimesheetController.java
@@ -2,30 +2,23 @@ package com.abdul.checkinservice.adapter.in.web;
 
 import com.abdul.checkinservice.domain.common.model.MessageDto;
 import com.abdul.checkinservice.domain.timesheet.model.TimeSheetDto;
-import com.abdul.checkinservice.domain.timesheet.port.in.FetchEmployeeTimeSheetUseCase;
 import com.abdul.checkinservice.domain.timesheet.port.in.RecordEmployeeTimeDataUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/timesheets")
 @RestController
 @RequiredArgsConstructor
 public class TimesheetController {
     private final RecordEmployeeTimeDataUseCase recordEmployeeTimeDataUseCase;
-    private final FetchEmployeeTimeSheetUseCase fetchEmployeeTimeSheetUseCase;
 
     @PostMapping("/{employeeId}/record")
     public ResponseEntity<MessageDto<TimeSheetDto>> recordEmployeeTimeData(@PathVariable Long employeeId) {
         return ResponseEntity.ok(recordEmployeeTimeDataUseCase.execute(employeeId));
-    }
-
-
-    @GetMapping("/{employeeId}/records")
-    public ResponseEntity<List<TimeSheetDto>> fetchEmployeeTimeData(@PathVariable Long employeeId) {
-        return ResponseEntity.ok(fetchEmployeeTimeSheetUseCase.allByEmployeeId(employeeId));
     }
 
 }
